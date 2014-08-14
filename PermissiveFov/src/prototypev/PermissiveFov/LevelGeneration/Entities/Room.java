8d494014b9072dd45896aa2afb8011d82455459f
Aaron Zhang <prototypev@gmail.com>
2014-08-14 15:21:13 -0400

    Add definition for a room.

---
+++ PermissiveFov/src/prototypev/PermissiveFov/LevelGeneration/Entities/Room.java
@@ -0,0 +1,269 @@
+package prototypev.PermissiveFov.LevelGeneration.Entities;
+
+import prototypev.PermissiveFov.LevelGeneration.DirectionType;
+import prototypev.PermissiveFov.LevelGeneration.SideType;
+import prototypev.PermissiveFov.Randomizer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Room {
+    public final int top;
+    public final int left;
+    public final int width;
+    public final int height;
+
+    private final Cell[][] cells;
+
+    /**
+     * Creates a new Room with the specified bounds.
+     *
+     * @param top    The top bound.
+     * @param left   The left bound.
+     * @param width  The number of cells spanning the room horizontally.
+     * @param height The number of cells spanning the room vertically.
+     */
+    private Room(int top, int left, int width, int height) {
+        if (top < 0 || left < 0 || width < 1 || height < 1) {
+            throw new IllegalArgumentException("Invalid bounds specified. top and left must be >= 0. width and height must be > 0");
+        }
+
+        this.top = top;
+        this.left = left;
+        this.width = width;
+        this.height = height;
+
+        cells = new Cell[height][width];
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                cells[y][x] = new Cell(left + x, top + y);
+            }
+        }
+    }
+
+    /**
+     * Creates a room filled with walls.
+     *
+     * @param top    The top bound.
+     * @param left   The left bound.
+     * @param width  The number of cells spanning the room horizontally.
+     * @param height The number of cells spanning the room vertically.
+     * @return The room.
+     */
+    public static Room createFilledRoom(int top, int left, int width, int height) {
+        return new Room(top, left, width, height);
+    }
+
+    /**
+     * Creates an empty room with walls only on the boundaries.
+     *
+     * @param top    The top bound.
+     * @param left   The left bound.
+     * @param width  The number of cells spanning the room horizontally.
+     * @param height The number of cells spanning the room vertically.
+     * @return The room.
+     */
+    public static Room createWalledInRoom(int top, int left, int width, int height) {
+        Room room = createFilledRoom(top, left, width, height);
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                Cell cell = room.getCellAt(x, y);
+
+                cell.setSide(DirectionType.NORTH, y == 0 ? SideType.WALL : SideType.EMPTY);
+                cell.setSide(DirectionType.WEST, x == 0 ? SideType.WALL : SideType.EMPTY);
+                cell.setSide(DirectionType.SOUTH, y == height - 1 ? SideType.WALL : SideType.EMPTY);
+                cell.setSide(DirectionType.EAST, x == width - 1 ? SideType.WALL : SideType.EMPTY);
+            }
+        }
+
+        return room;
+    }
+
+    /**
+     * Creates an empty room.
+     *
+     * @param top    The top bound.
+     * @param left   The left bound.
+     * @param width  The number of cells spanning the room horizontally.
+     * @param height The number of cells spanning the room vertically.
+     * @return The room.
+     */
+    public static Room createEmptyRoom(int top, int left, int width, int height) {
+        Room room = createFilledRoom(top, left, width, height);
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                Cell cell = room.getCellAt(x, y);
+
+                for (DirectionType direction : DirectionType.values()) {
+                    cell.setSide(direction, SideType.EMPTY);
+                }
+            }
+        }
+
+        return room;
+    }
+
+    /**
+     * @return The list of cells that have been visited.
+     */
+    public List<Cell> getVisitedCells() {
+        List<Cell> visitedCells = new ArrayList<Cell>();
+
+        for (Cell[] row : cells) {
+            for (Cell cell : row) {
+                if (cell.isVisited()) {
+                    visitedCells.add(cell);
+                }
+            }
+        }
+
+        return visitedCells;
+    }
+
+    /**
+     * @return true if all cells in this room have been visited; otherwise false.
+     */
+    public boolean isAllCellsVisited() {
+        for (Cell[] row : cells) {
+            for (Cell cell : row) {
+                if (!cell.isVisited()) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * @return The list of cells that are dead-ends.
+     * Cells with 3 wall sides will be dead-end cells.
+     */
+    public List<Cell> getDeadEndCells() {
+        List<Cell> deadEndCells = new ArrayList<Cell>();
+
+        for (Cell[] row : cells) {
+            for (Cell cell : row) {
+                if (cell.isDeadEnd()) {
+                    deadEndCells.add(cell);
+                }
+            }
+        }
+
+        return deadEndCells;
+    }
+
+    /**
+     * @return A cell at random.
+     */
+    public Cell getRandomCell() {
+        int x = Randomizer.getInstance().nextInt(width);
+        int y = Randomizer.getInstance().nextInt(height);
+
+        return getCellAt(x, y);
+    }
+
+    /**
+     * @param x The horizontal component.
+     * @param y The vertical component.
+     * @return The cell at the specified co-ordinates.
+     */
+    public Cell getCellAt(int x, int y) {
+        if (isOutOfBounds(x, y)) {
+            throw new IllegalStateException(String.format("(%d, %d) is out of bounds!", x, y));
+        }
+
+        return cells[y][x];
+    }
+
+    /**
+     * @param x The horizontal component.
+     * @param y The vertical component.
+     * @return true if the specified co-ordinates is outside the bounds of this room; otherwise false.
+     */
+    public boolean isOutOfBounds(int x, int y) {
+        return x < left || y < top || y > top + height || x > left + width;
+    }
+
+    /**
+     * @param x         The horizontal component.
+     * @param y         The vertical component.
+     * @param direction The direction to check.
+     * @return true if there is an adjacent cell in the specified direction from the specified co-ordinates; otherwise false.
+     */
+    public boolean hasAdjacentCell(int x, int y, DirectionType direction) {
+        if (isOutOfBounds(x, y)) {
+            throw new IllegalStateException(String.format("(%d, %d) is out of bounds!", x, y));
+        }
+
+        switch (direction) {
+            case NORTH:
+                return y > 0;
+
+            case WEST:
+                return x > 0;
+
+            case SOUTH:
+                return y < height - 1;
+
+            case EAST:
+                return x < width - 1;
+
+            default:
+                // This should not happen
+                throw new IllegalStateException(String.format("Direction %s is invalid.", direction.getName()));
+        }
+    }
+
+    /**
+     * @param x         The horizontal component.
+     * @param y         The vertical component.
+     * @param direction The direction to check.
+     * @return The cell adjacent from the specified co-ordinates in the specified direction.
+     * If no adjacent cells are available, returns null.
+     */
+    public Cell getAdjacentCell(int x, int y, DirectionType direction) {
+        if (!hasAdjacentCell(x, y, direction)) {
+            return null;
+        }
+
+        switch (direction) {
+            case NORTH:
+                return getCellAt(x, y - 1);
+
+            case WEST:
+                return getCellAt(x - 1, y);
+
+            case SOUTH:
+                return getCellAt(x, y + 1);
+
+            case EAST:
+                return getCellAt(x + 1, y);
+
+            default:
+                // This should not happen
+                throw new IllegalStateException(String.format("Direction %s is invalid.", direction.getName()));
+        }
+    }
+
+    /**
+     * Sets the side of the specified cell.
+     *
+     * @param x         The horizontal component.
+     * @param y         The vertical component.
+     * @param direction The direction to set.
+     * @param sideType  The side type to set.
+     * @return The adjacent cell in the specified direction.
+     */
+    public Cell setCellSide(int x, int y, DirectionType direction, SideType sideType) {
+        Cell cell = getCellAt(x, y);
+        cell.setSide(direction, sideType);
+
+        Cell adjacentCell = getAdjacentCell(x, y, direction);
+        adjacentCell.setSide(direction.getOpposite(), sideType);
+
+        return adjacentCell;
+    }
+}
---
+++ PermissiveFov/src/prototypev/PermissiveFov/Tests/LevelGeneration/Entities/RoomTests.java
@@ -0,0 +1,432 @@
+package prototypev.PermissiveFov.Tests.LevelGeneration.Entities;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import prototypev.PermissiveFov.LevelGeneration.DirectionType;
+import prototypev.PermissiveFov.LevelGeneration.Entities.Cell;
+import prototypev.PermissiveFov.LevelGeneration.Entities.Room;
+import prototypev.PermissiveFov.LevelGeneration.SideType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+@RunWith(JUnit4.class)
+public class RoomTests {
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void newRoom_InvalidArguments_ExpectsException() {
+        exception.expect(IllegalArgumentException.class);
+        Room.createFilledRoom(-1, -1, 0, 0);
+    }
+
+    @Test
+    public void hasAdjacentCell_OutOfBounds_ExpectsException() {
+        Room room = Room.createFilledRoom(0, 0, 1, 1);
+
+        exception.expect(IllegalStateException.class);
+        room.hasAdjacentCell(-1, -1, DirectionType.NORTH);
+    }
+
+    @Test
+    public void hasAdjacentCell_CellBesideBoundary_ExpectTrue() {
+        Room room = Room.createFilledRoom(0, 0, 3, 3);
+
+        for (DirectionType direction : DirectionType.values()) {
+            assertTrue(String.format("Cell (1, 1) should have an adjacent cell to the %s!", direction.getName()), room.hasAdjacentCell(1, 1, direction));
+        }
+    }
+
+    @Test
+    public void hasAdjacentCell_CellOnBoundary_ExpectsFalse() {
+        Room room = Room.createFilledRoom(0, 0, 1, 1);
+        for (DirectionType direction : DirectionType.values()) {
+            assertFalse(String.format("Cell (0, 0) should not have an adjacent cell to the %s!", direction.getName()), room.hasAdjacentCell(0, 0, direction));
+        }
+    }
+
+    @Test
+    public void setCellSide_MakeNorthCorridor_ExpectsCorrectLayout() {
+        Room room = Room.createFilledRoom(0, 0, 3, 3);
+        room.setCellSide(1, 1, DirectionType.NORTH, SideType.EMPTY);
+
+        /*
+         * Legend:
+         * X - SIDE_WALL
+         * O - SIDE_EMPTY
+         * . - Centre of cell
+         *
+         * Room after corridor is made should look like this:
+         *
+         *       0   1   2
+         *    --------------
+         *	  |	     X
+         *	0 |	    X.X
+         *	  |	     O
+         *    |
+         *	  |	 X   O   X
+         *	1 |	X.X X.X X.X
+         *	  |	 X   X   X
+         *    |
+         *	  |	     X
+         *	2 |	    X.X
+         *	  |	     X
+         */
+
+        Map<DirectionType, SideType> expectedSides = new HashMap<DirectionType, SideType>();
+
+        expectedSides.put(DirectionType.NORTH, SideType.EMPTY);
+        expectedSides.put(DirectionType.WEST, SideType.WALL);
+        expectedSides.put(DirectionType.SOUTH, SideType.WALL);
+        expectedSides.put(DirectionType.EAST, SideType.WALL);
+        assertSides(room, 1, 1, expectedSides);
+
+        expectedSides.put(DirectionType.NORTH, SideType.WALL);
+        assertSides(room, 0, 1, expectedSides);
+        assertSides(room, 1, 2, expectedSides);
+        assertSides(room, 2, 1, expectedSides);
+
+        expectedSides.put(DirectionType.SOUTH, SideType.EMPTY);
+        assertSides(room, 1, 0, expectedSides);
+    }
+
+    @Test
+    public void setCellSide_MakeSouthCorridor_ExpectsCorrectLayout() {
+        Room room = Room.createFilledRoom(0, 0, 3, 3);
+        room.setCellSide(1, 1, DirectionType.SOUTH, SideType.EMPTY);
+
+        /*
+         * Legend:
+         * X - SIDE_WALL
+         * O - SIDE_EMPTY
+         * . - Centre of cell
+         *
+         * Test grid after corridor is made should look like this:
+         *
+         *       0   1   2
+         *    --------------
+         *	  |	     X
+         *	0 |	    X.X
+         *	  |	     X
+         *    |
+         *	  |	 X   X   X
+         *	1 |	X.X X.X X.X
+         *	  |	 X   O   X
+         *    |
+         *	  |	     O
+         *	2 |	    X.X
+         *	  |	     X
+         */
+
+        Map<DirectionType, SideType> expectedSides = new HashMap<DirectionType, SideType>();
+
+        expectedSides.put(DirectionType.NORTH, SideType.WALL);
+        expectedSides.put(DirectionType.WEST, SideType.WALL);
+        expectedSides.put(DirectionType.SOUTH, SideType.EMPTY);
+        expectedSides.put(DirectionType.EAST, SideType.WALL);
+        assertSides(room, 1, 1, expectedSides);
+
+        expectedSides.put(DirectionType.SOUTH, SideType.WALL);
+        assertSides(room, 0, 1, expectedSides);
+        assertSides(room, 1, 0, expectedSides);
+        assertSides(room, 2, 1, expectedSides);
+
+        expectedSides.put(DirectionType.NORTH, SideType.EMPTY);
+        assertSides(room, 1, 2, expectedSides);
+    }
+
+    @Test
+    public void setCellSide_MakeWestCorridor_ExpectsCorrectLayout() {
+        Room room = Room.createFilledRoom(0, 0, 3, 3);
+        room.setCellSide(1, 1, DirectionType.WEST, SideType.EMPTY);
+
+        /*
+         * Legend:
+         * X - SIDE_WALL
+         * O - SIDE_EMPTY
+         * . - Centre of cell
+         *
+         * Test grid after corridor is made should look like this:
+         *
+         *       0   1   2
+         *    --------------
+         *	  |	     X
+         *	0 |	    X.X
+         *	  |	     X
+         *    |
+         *	  |	 X   X   X
+         *	1 |	X.O O.X X.X
+         *	  |	 X   X   X
+         *    |
+         *	  |	     X
+         *	2 |	    X.X
+         *	  |	     X
+         */
+
+        Map<DirectionType, SideType> expectedSides = new HashMap<DirectionType, SideType>();
+
+        expectedSides.put(DirectionType.NORTH, SideType.WALL);
+        expectedSides.put(DirectionType.WEST, SideType.EMPTY);
+        expectedSides.put(DirectionType.SOUTH, SideType.WALL);
+        expectedSides.put(DirectionType.EAST, SideType.WALL);
+        assertSides(room, 1, 1, expectedSides);
+
+        expectedSides.put(DirectionType.WEST, SideType.WALL);
+        assertSides(room, 1, 0, expectedSides);
+        assertSides(room, 1, 2, expectedSides);
+        assertSides(room, 2, 1, expectedSides);
+
+        expectedSides.put(DirectionType.EAST, SideType.EMPTY);
+        assertSides(room, 0, 1, expectedSides);
+    }
+
+    @Test
+    public void setCellSide_MakeEastCorridor_ExpectsCorrectLayout() {
+        Room room = Room.createFilledRoom(0, 0, 3, 3);
+        room.setCellSide(1, 1, DirectionType.EAST, SideType.EMPTY);
+
+        /*
+         * Legend:
+         * X - SIDE_WALL
+         * O - SIDE_EMPTY
+         * . - Centre of cell
+         *
+         * Test grid after corridor is made should look like this:
+         *
+         *       0   1   2
+         *    --------------
+         *	  |	     X
+         *	0 |	    X.X
+         *	  |	     X
+         *    |
+         *	  |	 X   X   X
+         *	1 |	X.X X.O O.X
+         *	  |	 X   X   X
+         *    |
+         *	  |	     X
+         *	2 |	    X.X
+         *	  |	     X
+         */
+
+        Map<DirectionType, SideType> expectedSides = new HashMap<DirectionType, SideType>();
+
+        expectedSides.put(DirectionType.NORTH, SideType.WALL);
+        expectedSides.put(DirectionType.WEST, SideType.WALL);
+        expectedSides.put(DirectionType.SOUTH, SideType.WALL);
+        expectedSides.put(DirectionType.EAST, SideType.EMPTY);
+        assertSides(room, 1, 1, expectedSides);
+
+        expectedSides.put(DirectionType.EAST, SideType.WALL);
+        assertSides(room, 1, 0, expectedSides);
+        assertSides(room, 1, 2, expectedSides);
+        assertSides(room, 0, 1, expectedSides);
+
+        expectedSides.put(DirectionType.WEST, SideType.EMPTY);
+        assertSides(room, 2, 1, expectedSides);
+    }
+
+    @Test
+    public void setCellSide_MakeNorthWall_ExpectsCorrectLayout() {
+        Room room = Room.createEmptyRoom(0, 0, 3, 3);
+        room.setCellSide(1, 1, DirectionType.NORTH, SideType.WALL);
+
+        /*
+         * Legend:
+         * X - SIDE_WALL
+         * O - SIDE_EMPTY
+         * . - Centre of cell
+         *
+         * Test grid after wall is made should look like this:
+         *
+         *       0   1   2
+         *    --------------
+         *	  |	     O
+         *	0 |	    O.O
+         *	  |	     X
+         *    |
+         *	  |	 O   X   O
+         *	1 |	O.O O.O O.O
+         *	  |	 O   O   O
+         *    |
+         *	  |	     O
+         *	2 |	    O.O
+         *	  |	     O
+         */
+
+        Map<DirectionType, SideType> expectedSides = new HashMap<DirectionType, SideType>();
+
+        expectedSides.put(DirectionType.NORTH, SideType.WALL);
+        expectedSides.put(DirectionType.WEST, SideType.EMPTY);
+        expectedSides.put(DirectionType.SOUTH, SideType.EMPTY);
+        expectedSides.put(DirectionType.EAST, SideType.EMPTY);
+        assertSides(room, 1, 1, expectedSides);
+
+        expectedSides.put(DirectionType.NORTH, SideType.EMPTY);
+        assertSides(room, 0, 1, expectedSides);
+        assertSides(room, 1, 2, expectedSides);
+        assertSides(room, 2, 1, expectedSides);
+
+        expectedSides.put(DirectionType.SOUTH, SideType.WALL);
+        assertSides(room, 1, 0, expectedSides);
+    }
+
+    @Test
+    public void setCellSide_MakeSouthWall_ExpectsCorrectLayout() {
+        Room room = Room.createEmptyRoom(0, 0, 3, 3);
+        room.setCellSide(1, 1, DirectionType.SOUTH, SideType.WALL);
+
+        /*
+         * Legend:
+         * X - SIDE_WALL
+         * O - SIDE_EMPTY
+         * . - Centre of cell
+         *
+         * Test grid after wall is made should look like this:
+         *
+         *       0   1   2
+         *    --------------
+         *	  |	     O
+         *	0 |	    O.O
+         *	  |	     O
+         *    |
+         *	  |	 O   O   O
+         *	1 |	O.O O.O O.O
+         *	  |	 O   X   O
+         *    |
+         *	  |	     X
+         *	2 |	    O.O
+         *	  |	     O
+         */
+
+        Map<DirectionType, SideType> expectedSides = new HashMap<DirectionType, SideType>();
+
+        expectedSides.put(DirectionType.NORTH, SideType.EMPTY);
+        expectedSides.put(DirectionType.WEST, SideType.EMPTY);
+        expectedSides.put(DirectionType.SOUTH, SideType.WALL);
+        expectedSides.put(DirectionType.EAST, SideType.EMPTY);
+        assertSides(room, 1, 1, expectedSides);
+
+        expectedSides.put(DirectionType.SOUTH, SideType.EMPTY);
+        assertSides(room, 0, 1, expectedSides);
+        assertSides(room, 1, 0, expectedSides);
+        assertSides(room, 2, 1, expectedSides);
+
+        expectedSides.put(DirectionType.NORTH, SideType.WALL);
+        assertSides(room, 1, 2, expectedSides);
+    }
+
+    @Test
+    public void setCellSide_MakeWestWall_ExpectsCorrectLayout() {
+        Room room = Room.createEmptyRoom(0, 0, 3, 3);
+        room.setCellSide(1, 1, DirectionType.WEST, SideType.WALL);
+
+        /*
+         * Legend:
+         * X - SIDE_WALL
+         * O - SIDE_EMPTY
+         * . - Centre of cell
+         *
+         * Test grid after wall is made should look like this:
+         *
+         *       0   1   2
+         *    --------------
+         *	  |	     O
+         *	0 |	    O.O
+         *	  |	     O
+         *    |
+         *	  |	 O   O   O
+         *	1 |	O.X X.O O.O
+         *	  |	 O   O   O
+         *    |
+         *	  |	     O
+         *	2 |	    O.O
+         *	  |	     O
+         */
+
+        Map<DirectionType, SideType> expectedSides = new HashMap<DirectionType, SideType>();
+
+        expectedSides.put(DirectionType.NORTH, SideType.EMPTY);
+        expectedSides.put(DirectionType.WEST, SideType.WALL);
+        expectedSides.put(DirectionType.SOUTH, SideType.EMPTY);
+        expectedSides.put(DirectionType.EAST, SideType.EMPTY);
+        assertSides(room, 1, 1, expectedSides);
+
+        expectedSides.put(DirectionType.WEST, SideType.EMPTY);
+        assertSides(room, 1, 2, expectedSides);
+        assertSides(room, 1, 0, expectedSides);
+        assertSides(room, 2, 1, expectedSides);
+
+        expectedSides.put(DirectionType.EAST, SideType.WALL);
+        assertSides(room, 0, 1, expectedSides);
+    }
+
+    @Test
+    public void setCellSide_MakeEastWall_ExpectsCorrectLayout() {
+        Room room = Room.createEmptyRoom(0, 0, 3, 3);
+        room.setCellSide(1, 1, DirectionType.EAST, SideType.WALL);
+
+        /*
+         * Legend:
+         * X - SIDE_WALL
+         * O - SIDE_EMPTY
+         * . - Centre of cell
+         *
+         * Test grid after wall is made should look like this:
+         *
+         *       0   1   2
+         *    --------------
+         *	  |	     O
+         *	0 |	    O.O
+         *	  |	     O
+         *    |
+         *	  |	 O   O   O
+         *	1 |	O.O O.X X.O
+         *	  |	 O   O   O
+         *    |
+         *	  |	     O
+         *	2 |	    O.O
+         *	  |	     O
+         */
+
+        Map<DirectionType, SideType> expectedSides = new HashMap<DirectionType, SideType>();
+
+        expectedSides.put(DirectionType.NORTH, SideType.EMPTY);
+        expectedSides.put(DirectionType.WEST, SideType.EMPTY);
+        expectedSides.put(DirectionType.SOUTH, SideType.EMPTY);
+        expectedSides.put(DirectionType.EAST, SideType.WALL);
+        assertSides(room, 1, 1, expectedSides);
+
+        expectedSides.put(DirectionType.EAST, SideType.EMPTY);
+        assertSides(room, 1, 2, expectedSides);
+        assertSides(room, 1, 0, expectedSides);
+        assertSides(room, 0, 1, expectedSides);
+
+        expectedSides.put(DirectionType.WEST, SideType.WALL);
+        assertSides(room, 2, 1, expectedSides);
+    }
+
+    /**
+     * Asserts that all sides of the cell at the specified co-ordinates in the room matches the expectation.
+     *
+     * @param room          The containing room.
+     * @param x             The horizontal component.
+     * @param y             The vertical component.
+     * @param expectedSides The expected sides.
+     */
+    private void assertSides(Room room, int x, int y, Map<DirectionType, SideType> expectedSides) {
+        Cell cell = room.getCellAt(x, y);
+
+        for (Map.Entry<DirectionType, SideType> entry : expectedSides.entrySet()) {
+            DirectionType direction = entry.getKey();
+            SideType expectedSide = entry.getValue();
+            SideType actualSide = cell.getSide(direction);
+            assertEquals(String.format("%s side of (%d, %d) should be %s!", direction.getName(), x, y, expectedSide.getName()), expectedSide, actualSide);
+        }
+    }
+}
