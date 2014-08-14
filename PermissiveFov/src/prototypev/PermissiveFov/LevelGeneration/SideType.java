437ba547527708d007b3d7c0947b3d1e00048a65
Aaron Zhang <prototypev@gmail.com>
2014-08-14 14:37:11 -0400

    Add name to SideType

--- PermissiveFov/src/prototypev/PermissiveFov/LevelGeneration/SideType.java
+++ PermissiveFov/src/prototypev/PermissiveFov/LevelGeneration/SideType.java
@@ -1,15 +1,21 @@
 package prototypev.PermissiveFov.LevelGeneration;
 
 public enum SideType {
-    WALL(0), EMPTY(1), DOOR(2);
+    WALL(0, "Wall"), EMPTY(1, "Empty"), DOOR(2, "Door");
 
     private final int value;
+    private final String name;
 
-    private SideType(int value) {
+    private SideType(int value, String name) {
         this.value = value;
+        this.name = name;
     }
 
     public int getValue() {
         return value;
+    }
+
+    public String getName() {
+        return name;
     }
 }
