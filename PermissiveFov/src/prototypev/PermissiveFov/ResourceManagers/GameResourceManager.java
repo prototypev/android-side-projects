25e20c7ee1acdb299239d13d7150373b477bdc42
Aaron Zhang <prototypev@gmail.com>
2014-08-20 20:48:03 -0400

    Update tiled map with generated level data

--- PermissiveFov/src/prototypev/PermissiveFov/ResourceManagers/GameResourceManager.java
+++ PermissiveFov/src/prototypev/PermissiveFov/ResourceManagers/GameResourceManager.java
@@ -1,31 +1,234 @@
 package prototypev.PermissiveFov.ResourceManagers;
 
-import org.andengine.extension.tmx.TMXLoader;
-import org.andengine.extension.tmx.TMXTiledMap;
+import org.andengine.extension.tmx.*;
 import org.andengine.ui.activity.BaseGameActivity;
+import prototypev.PermissiveFov.LevelGeneration.Entities.Level;
+import prototypev.PermissiveFov.LevelGeneration.TileType;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class GameResourceManager implements IResourceManager {
+    private static final int[][] adjacentTileDeltasIncludeDiagonals = new int[][]
+            {
+                    {-1, -1}, {0, -1}, {1, -1},
+                    {-1, 0}, {1, 0},
+                    {-1, 1}, {0, 1}, {1, 1}
+            };
+    private static final int[][] adjacentTileDeltasNoDiagonals = new int[][]
+            {
+                    {0, -1},
+                    {-1, 0}, {1, 0},
+                    {0, 1}
+            };
+    private static final String WILDCARD_STRING = "*";
     private static final String MAP_FILE = "map.tmx";
 
     private final BaseGameActivity activity;
+
     private TMXTiledMap tiledMap;
+    private Map<String, Integer> backgroundTiles;
+    private int collidableTileID;
 
     public GameResourceManager(BaseGameActivity activity) {
         this.activity = activity;
+    }
+
+    /**
+     * @param level The generated level.
+     * @param x     The horizontal component of the co-ordinate.
+     * @param y     The vertical component of the co-ordinate.
+     * @return The binary string representation of the wall type at the specified co-ordinates.
+     */
+    private static String getWallType(Level level, int x, int y) {
+        int wallType = 0xFF; // Initialize to 11111111
+        for (int i = 0; i < adjacentTileDeltasIncludeDiagonals.length; i++) {
+            int adjX = x + adjacentTileDeltasIncludeDiagonals[i][0];
+            int adjY = y + adjacentTileDeltasIncludeDiagonals[i][1];
+
+            // Skip if out of bounds
+            if (!level.isOutOfBounds(adjX, adjY)) {
+                // To get the wall type, we do an XOR on the i-th most significant bit
+                TileType adjTileType = level.getTileTypeAt(adjX, adjY);
+                if (adjTileType == TileType.EMPTY || adjTileType == TileType.DOOR) {
+                    wallType ^= 1 << (adjacentTileDeltasIncludeDiagonals.length - 1 - i);
+                }
+            }
+        }
+
+        return String.format("%8s", Integer.toBinaryString(wallType)).replace(' ', '0');
+    }
+
+    /**
+     * @param level The generated level.
+     * @param x     The horizontal component of the co-ordinate.
+     * @param y     The vertical component of the co-ordinate.
+     * @return The key representing the wall type at the specified co-ordinates.
+     */
+    private static String getWallKey(Level level, int x, int y) {
+        // Get the wall type
+        String binaryString = getWallType(level, x, y);
+
+        return String.format("%s_%s", TileType.WALL.getName(), binaryString);
+    }
+
+    /**
+     * @param level The generated level.
+     * @param x     The horizontal component of the co-ordinate.
+     * @param y     The vertical component of the co-ordinate.
+     * @return The background tile ID.
+     */
+    public int getBackgroundTileIDForWall(Level level, int x, int y) {
+        String wallKey = getWallKey(level, x, y);
+
+        if (backgroundTiles.containsKey(wallKey)) {
+            return backgroundTiles.get(wallKey);
+        }
+
+        // Attempt wildcard match
+        Set<String> keys = backgroundTiles.keySet();
+        for (String key : keys) {
+            // Skip if key does not allow wildcards
+            if (key.contains(WILDCARD_STRING)) {
+                // Replace WILDCARD_STRING with \d to sniff only digits
+                String regex = key.replace(WILDCARD_STRING, "\\d");
+
+                Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+                Matcher matcher = pattern.matcher(wallKey);
+                if (matcher.matches()) {
+                    return backgroundTiles.get(key);
+                }
+            }
+        }
+
+        // Not a special wall
+        return backgroundTiles.get(TileType.WALL.getName());
+    }
+
+    /**
+     * @param tileID       The tile ID.
+     * @param propertyName The name of the property to get.
+     * @return The value of the property.
+     */
+    private String getTilePropertyValue(int tileID, String propertyName) {
+        TMXProperties<TMXTileProperty> properties = tiledMap.getTMXTileProperties(tileID);
+        if (properties != null) {
+            for (TMXTileProperty property : properties) {
+                if (property.getName().equals(propertyName)) {
+                    return property.getValue();
+                }
+            }
+        }
+
+        throw new IllegalArgumentException(String.format("No property with name: %s found for tile ID: %d!", propertyName, tileID));
     }
 
     @Override
     public void load() throws Exception {
         TMXLoader tmxLoader = new TMXLoader(activity.getAssets(), activity.getTextureManager(), activity.getVertexBufferObjectManager());
         tiledMap = tmxLoader.loadFromAsset(MAP_FILE);
+
+        backgroundTiles = readBackgroundTiles();
+        collidableTileID = readCollidableTileID();
     }
 
     @Override
     public void unload() {
-        // TODO
+        backgroundTiles = null;
+        tiledMap = null;
     }
 
     public TMXTiledMap getTiledMap() {
         return tiledMap;
+    }
+
+    public Map<String, Integer> getBackgroundTiles() {
+        return backgroundTiles;
+    }
+
+    public int getCollidableTileID() {
+        return collidableTileID;
+    }
+
+    public int getWalkableTileID() {
+        // Assumption: the tile IDs are sequential, and the collidable meta tile comes first.
+        return collidableTileID + 1;
+    }
+
+    /**
+     * @param layerName The layer name.
+     * @return The layer, if found.
+     */
+    public TMXLayer getLayer(String layerName) {
+        ArrayList<TMXLayer> layers = tiledMap.getTMXLayers();
+
+        for (TMXLayer layer : layers) {
+            if (layer.getName().equals(layerName)) {
+                return layer;
+            }
+        }
+
+        throw new IllegalArgumentException(String.format("Layer: '%s' not found!", layerName));
+    }
+
+    /**
+     * @return The background tiles.
+     */
+    private Map<String, Integer> readBackgroundTiles() {
+        Map<String, Integer> tiles = new HashMap<String, Integer>();
+
+        TMXLayer backgroundLayer = getLayer("Background");
+
+        boolean isScanEnded = false;
+
+        int rows = tiledMap.getTileRows();
+        int columns = tiledMap.getTileColumns();
+
+        // Loop through the tiled map to retrieve the tile ID associations
+        for (int y = 0; !isScanEnded && y < rows; y++) {
+            for (int x = 0; !isScanEnded && x < columns; x++) {
+                TMXTile tile = backgroundLayer.getTMXTile(x, y);
+                int tileID = tile.getGlobalTileID();
+
+                try {
+                    String tileType = getTilePropertyValue(tileID, "TileType");
+                    tiles.put(tileType, tileID);
+                } catch (IllegalArgumentException ex) {
+                    // If tile has no properties, we are done (the rest are fillers)
+                    isScanEnded = true;
+                }
+            }
+        }
+
+        return tiles;
+    }
+
+    /**
+     * @return The collidable tile ID.
+     */
+    private int readCollidableTileID() {
+        TMXLayer metaLayer = getLayer("Meta");
+
+        int rows = tiledMap.getTileRows();
+        int columns = tiledMap.getTileColumns();
+
+        // Loop through the tiled map to retrieve the tile ID associations
+        for (int y = 0; y < rows; y++) {
+            for (int x = 0; x < columns; x++) {
+                TMXTile tile = metaLayer.getTMXTile(x, y);
+                int tileID = tile.getGlobalTileID();
+
+                boolean isCollidable = Boolean.parseBoolean(getTilePropertyValue(tileID, "Collidable"));
+                if (isCollidable) {
+                    return tile.getGlobalTileID();
+                }
+            }
+        }
+
+        throw new IllegalArgumentException("No collidable tile found in meta layer!");
     }
 }
--- PermissiveFov/src/prototypev/PermissiveFov/Scenes/GameScene.java
+++ PermissiveFov/src/prototypev/PermissiveFov/Scenes/GameScene.java
@@ -1,24 +1,99 @@
 package prototypev.PermissiveFov.Scenes;
 
+import org.andengine.extension.tmx.TMXLayer;
+import org.andengine.extension.tmx.TMXTile;
 import org.andengine.extension.tmx.TMXTiledMap;
 import org.andengine.ui.activity.BaseGameActivity;
 import prototypev.PermissiveFov.LevelGeneration.Entities.Level;
 import prototypev.PermissiveFov.LevelGeneration.Generators.LevelGenerator;
 import prototypev.PermissiveFov.LevelGeneration.Generators.MazeGenerator;
 import prototypev.PermissiveFov.LevelGeneration.Generators.RoomGenerator;
+import prototypev.PermissiveFov.LevelGeneration.TileType;
 import prototypev.PermissiveFov.ResourceManagers.GameResourceManager;
 
+import java.util.Map;
+
 public class GameScene extends SceneBase {
+    private int spawnX;
+    private int spawnY;
+
     public GameScene(BaseGameActivity activity, GameResourceManager resourceManager) {
         super(activity, resourceManager);
 
+        TMXTiledMap tiledMap = resourceManager.getTiledMap();
+        Level level = generateLevel(tiledMap);
+
+        updateTiledMap(tiledMap, level, resourceManager);
+
+        generateSpawnPoint(level);
+    }
+
+    /**
+     * Generates a random level and tile it.
+     *
+     * @param tiledMap The tiled map.
+     */
+    private static Level generateLevel(TMXTiledMap tiledMap) {
         // Generate the random level
         MazeGenerator mazeGenerator = new MazeGenerator(30, 70);
         RoomGenerator roomGenerator = new RoomGenerator(2, 3, 2, 3);
-        Level level = LevelGenerator.generate(15, 15, mazeGenerator, roomGenerator, 5);
-
-        // Set the appropriate tiles in the tiled map based on what was generated
-        TMXTiledMap tiledMap = resourceManager.getTiledMap();
+        return LevelGenerator.generate(tiledMap.getTileColumns() / 2, tiledMap.getTileRows() / 2, mazeGenerator, roomGenerator, 5);
     }
 
+    /**
+     * Sets the appropriate tiles in the tiled map based on what was generated.
+     *
+     * @param tiledMap        The tiled map.
+     * @param level           The generated level.
+     * @param resourceManager The resource manager.
+     */
+    private static void updateTiledMap(TMXTiledMap tiledMap, Level level, GameResourceManager resourceManager) {
+        TMXLayer backgroundLayer = resourceManager.getLayer("Background");
+        TMXLayer metaLayer = resourceManager.getLayer("Meta");
+
+        Map<String, Integer> backgroundTiles = resourceManager.getBackgroundTiles();
+        int collidableTileID = resourceManager.getCollidableTileID();
+        int walkableTileID = resourceManager.getWalkableTileID();
+
+        for (int y = 0; y < level.height; y++) {
+            for (int x = 0; x < level.width; x++) {
+                TMXTile backgroundTile = backgroundLayer.getTMXTile(x, y);
+                TMXTile metaTile = metaLayer.getTMXTile(x, y);
+
+                TileType tileType = level.getTileTypeAt(x, y);
+                int backgroundTileID;
+
+                if (tileType == TileType.WALL) {
+                    // Mark meta layer tile as collidable if it's a wall
+                    metaTile.setGlobalTileID(tiledMap, collidableTileID);
+
+                    backgroundTileID = resourceManager.getBackgroundTileIDForWall(level, x, y);
+                } else {
+                    // Non-wall, mark meta tile as walkable
+                    metaTile.setGlobalTileID(tiledMap, walkableTileID);
+
+                    backgroundTileID = backgroundTiles.get(tileType.getName());
+                }
+
+                // Set the background tile accordingly
+                backgroundTile.setGlobalTileID(tiledMap, backgroundTileID);
+            }
+        }
+    }
+
+    /**
+     * Generates the player's spawn point.
+     *
+     * @param level The generated level.
+     */
+    private void generateSpawnPoint(Level level) {
+        // TODO: For now, just place the player on the first empty tile.
+        for (spawnY = 0; spawnY < level.height; spawnY++) {
+            for (spawnX = 0; spawnX < level.width; spawnX++) {
+                if (level.getTileTypeAt(spawnX, spawnY) == TileType.EMPTY) {
+                    break;
+                }
+            }
+        }
+    }
 }
